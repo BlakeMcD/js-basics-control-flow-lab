7ce5e55d6845b7903f3f5ea22e1fd67691580959
BlakeMcD <blakemcd@gmail.com>
2021-07-09 16:40:40 +1000

    Assignment Complete

--- index.js
+++ index.js
@@ -15,15 +15,15 @@ function ternaryCheckCity(city){
 
 
 function switchOnCharmFromTip(tip){
-  if tip == 'generous' {
-    return 'Thank you so much'
+  if (tip == 'generous') {
+    return 'Thank you so much.'
   }
-  else if tip == 'not as generous' {
+  else if (tip == 'not as generous') {
     return 'Thank you.'
   }
   else 
   {
-    return 'bye'
+    return 'Bye.'
   }
   // Write your code here!
   /*
